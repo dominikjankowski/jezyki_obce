77591213bd5dac27bfab643c02798700137cf071
dominikjankowski <dominik.jankowsk@gmail.com>
2018-08-30 08:51:57 +0200

    prace nad sekcjią header

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+const gulp          = require("gulp");
+const sass          = require("gulp-sass");
+const sourcemaps    = require("gulp-sourcemaps");
+const autoprefixer  = require("gulp-autoprefixer");
+const plumber       = require("gulp-plumber");
+const colors        = require("ansi-colors");
+const notifier      = require("node-notifier")
+const rename        = require("gulp-rename");
+const wait          = require("gulp-wait");
+const csso          = require("gulp-csso");
+const htmlMin       = require("gulp-htmlmin");
+const browserSync   = require("browser-sync").create();
+const webpack       = require("webpack");
+
+function showError(err) {
+    // notifier.notify({
+    //     title: "Error in sass",
+    //     message: err.messageFormatted
+    //   });
+
+    console.log(colors.red("==============================="));
+    console.log(colors.red(err.messageFormatted));
+    console.log(colors.red("==============================="));
+    this.emit("end");
+}
+
+
+gulp.task("browserSync", function() {
+    browserSync.init({
+        server: "dist",
+        notify: false,
+        open: true,
+       
+    });
+});
+
+gulp.task("html", function(){
+    return gulp.src("src/html/**/*.html")
+        .pipe(plumber({
+            errorHandler: showError
+        }))
+        .pipe(htmlMin({collapseWhitespace: true}))
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task("sass", function() {
+    return gulp.src("src/scss/main.scss")
+        .pipe(wait(500))
+        .pipe(plumber({
+            errorHandler: showError
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "compressed" //nested, expanded, compact, compressed
+        }))
+        .pipe(autoprefixer({
+            browsers: ["> 5%"]
+        })) //autoprefixy https://github.com/browserslist/browserslist#queries
+        .pipe(csso())
+        .pipe(rename({
+            suffix: ".min",
+            basename: "style"
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream({match: "**/*.css"}));
+});
+
+
+gulp.task("es6", function(cb) { //https://github.com/webpack/docs/wiki/usage-with-gulp#normal-compilation
+    return webpack(require("./webpack.config.js"), function(err, stats) {
+        if (err) throw err;
+        console.log(stats.toString());
+        cb();
+        browserSync.reload();
+    })
+})
+
+
+gulp.task("img", function() {
+    return gulp.src("src/img/*.+(png|jpg|jpeg|gif|svg)")
+    .pipe(gulp.dest("dist/img"))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+})
+
+gulp.task("watch", function() {
+    gulp.watch("src/scss/**/*.scss", ["sass"]);
+    gulp.watch("src/js/**/*.js", ["es6"]);
+    gulp.watch("src/html/**/*.html", ["html"]);
+});
+
+
+gulp.task("default", ["html", "sass", "es6", "img" ,"browserSync", "watch"]);
---
+++ src/js/test.js
@@ -0,0 +1,13 @@
+let obj = {
+	print(msg = "Domyslna wiadomosc") {
+		console.log(msg)
+	}
+}
+
+export { obj }
+
+const zbyszek = 33;
+console.log(zbyszek);
+
+const jasia= 66;
+console.log(jasia);
