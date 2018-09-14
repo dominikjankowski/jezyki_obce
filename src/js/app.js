ebff64d0c8b32497684796f0d2f6ef1d376b565c
dominikjankowski <dominik.jankowsk@gmail.com>
2018-09-14 22:30:29 +0200

    dodanie modali

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,37 @@
-import { obj } from './test';
+// import { obj } from './test';
 
 document.addEventListener('DOMContentLoaded', function() {
 
     //tutaj skrypty
-    obj.print("To jest wiadomosc bardzo ważna");
-    obj.print();
 
+    //MODAL 
+    let figureFirst = document.getElementById("figure-1");
+    let figureSecond = document.getElementById("figure-2");
+    let modalFirst = document.getElementById("modal-1");
+    let modalSecond = document.getElementById("modal-2");
+    let closeFirst = document.getElementById("close-1");
+    let closeSecond = document.getElementById("close-2");
+
+    figureFirst.onclick = function() {
+        modalFirst.style.display = "flex";
+    }
+    closeFirst.onclick = function() {
+        modalFirst.style.display = "none";
+    }
+
+    figureSecond.onclick = function() {
+        modalSecond.style.display = "flex";
+    }
+    closeSecond.onclick = function() {
+        modalSecond.style.display = "none";
+    }
+
+    window.onclick = function(event1) {
+        if (event1.target == modalFirst) {
+            modalFirst.style.display = "none";
+        }
+        else if (event1.target == modalSecond) {
+            modalSecond.style.display = "none";
+        }
+    }
 });
--- src/js/test.js
+++ src/js/test.js
@@ -1,13 +1,13 @@
-let obj = {
-	print(msg = "Domyslna wiadomosc") {
-		console.log(msg)
-	}
-}
+// let obj = {
+// 	print(msg = "Domyslna wiadomosc") {
+// 		console.log(msg)
+// 	}
+// }
 
-export { obj }
+// export { obj }
 
-const zbyszek = 33;
-console.log(zbyszek);
+// const zbyszek = 33;
+// console.log(zbyszek);
 
-const jasia= 66;
-console.log(jasia);
+// const jasia= 66;
+// console.log(jasia);
